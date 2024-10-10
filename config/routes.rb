fae8c2751112f4972b233651adb8d1bf2232b7c3
imaoka-yoshiko-98 <jiazijingang@gmail.com>
2024-10-10 21:11:44 +0900

    messerseのコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  before_action :authenticate_user!
+  devise_for :users
+  get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "messages#index"
 end
