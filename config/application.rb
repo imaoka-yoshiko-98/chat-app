a09c0b4e07ac31b32d1cc42e0b9539d14c6205f7
imaoka-yoshiko-98 <jiazijingang@gmail.com>
2024-10-16 11:25:08 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module ChatApp
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-   
+    config.active_storage.variant_processor = :mini_magick
+ 
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
